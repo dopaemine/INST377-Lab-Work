734cc122f9956a6df658353e4ffe6790e252051f
Dopa-min <60413498+dopaemine@users.noreply.github.com>
2022-05-09 16:31:30 -0400

    Labs complete

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -11,12 +11,39 @@ function getRandomIntInclusive(min, max) {
       const restNum = getRandomIntInclusive(0, dataArray.length - 1);
       return dataArray[restNum];
     });
-  
+
     return listItems;
   }
   
+  function initMap() {
+    const latLong = [38.784, -76.872];
+    const map = L.map('map').setView(latLong, 11);
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
+    }).addTo(map);
+    return map;
+  }
+  
+  function addMapMarkers(map, collection) {
+    map.eachLayer((layer) => {
+      if (layer instanceof L.Marker) {
+        layer.remove();
+      }
+    });
+  
+    collection.forEach((item) => {
+      const point = item.geocoded_column_1?.coordinates;
+      console.log(item.geocoded_column_1?.coordinates);
+      L.marker([point[1], point[0]]).addTo(map);
+    });
+  }
+  
   function createHtmlList(collection) {
-    console.log(collection);
     const targetList = document.querySelector('.resto-list');
     targetList.innerHTML = '';
     collection.forEach((item) => {
@@ -28,18 +55,26 @@ function getRandomIntInclusive(min, max) {
   }
   
   async function mainEvent() {
-    console.log('script loaded');
     const form = document.querySelector('.hunger-form');
     const submit = document.querySelector('.submit');
   
     const resto = document.querySelector('#resto_name');
     const zipcode = document.querySelector('#zipcode');
+    const map = initMap('map');
+    const retrievalVar = 'restaurants';
     submit.style.display = 'none';
   
-    const results = await fetch('/api/foodServicesPG'); 
-    const arrayFromJson = await results.json(); 
+    if (!localStorage.getItem(retrievalVar)) {
+      const results = await fetch('/api/foodServicesPG'); 
+      const arrayFromJson = await results.json(); 
+      localStorage.setItem(retrievalVar, JSON.stringify(arrayFromJson.data));
+    }
   
-    if (arrayFromJson.data.length > 0) {
+    const storedDataString = localStorage.getItem(retrievalVar);
+    const storedDataArray = JSON.parse(storedDataString);
+    console.log(storedDataArray);
+  
+    if (storedDataString?.length > 0) {
       submit.style.display = 'block';
   
       let currentArray = [];
@@ -48,7 +83,7 @@ function getRandomIntInclusive(min, max) {
           return;
         }
   
-        const selectResto = currentArray.filter((item) => {
+        const selectResto = storedDataArray.data.filter((item) => {
           const lowerName = item.name.toLowerCase();
           const lowerValue = event.target.value.toLowerCase();
           return lowerName.includes(lowerValue);
@@ -67,15 +102,12 @@ function getRandomIntInclusive(min, max) {
       });
   
       form.addEventListener('submit', async (submitEvent) => {
-        submitEvent.preventDefault();
-        // console.log('form submission'); // this is substituting for a "breakpoint"
-        // arrayFromJson.data - we're accessing a key called 'data' on the returned object
-        // it contains all 1,000 records we need
-        currentArray = restArrayMake(arrayFromJson.data);
-        console.log(currentArray);
+        submitEvent.preventDefault(); 
+        currentArray = restArrayMake(storedDataArray.data);
         createHtmlList(currentArray);
+  
+        addMapMarkers(map, currentArray);
       });
     }
   }
-  // this actually runs first! It's calling the function above
-  document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
+  document.addEventListener('DOMContentLoaded', async () => mainEvent());
